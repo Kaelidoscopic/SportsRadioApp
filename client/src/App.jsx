b438b5830c039fef464d3673c0f87f2e2a505836
Kaelidoscopic <nintenboy10@gmail.com>
2026-05-06 02:29:51 -0400

    Venue removal 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -544,7 +544,6 @@ function App() {
     return (
       <HostDashboard
         currentRoom={currentRoom}
-        venueId={venueId}
         isBroadcasting={isBroadcasting}
         isMuted={isMuted}
         leaveRoom={leaveRoom}
